d380eaf634afd551bdf7e5fedb425e51327c15f9
Marcnuth <hxianxian@qq.com>
2016-06-11 23:35:49 +0800

    change the words count

--- websites_keywords_extractor/pipelines.py
+++ websites_keywords_extractor/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sqlite3, time, jieba, jieba.analyse
+import sqlite3, time, jieba, jieba.analyse, jieba.posseg 
 import settings as conf
 
 class SaveToSqlitePipeline(object):
@@ -18,8 +18,13 @@ class SaveToSqlitePipeline(object):
             cursor = conn.cursor()
             cursor.execute('INSERT INTO urls VALUES(?, ?)', (item['url'], int(time.time())))
 
-            words = jieba.analyse.extract_tags(item['content'], topK=20, allowPOS=('n'))
-            for word in words:
+            for word, flag in jieba.posseg.cut(item['content']):
+                if len(word) < 2:
+                    continue
+
+                if flag in ['r', 'nr', 'eng', 'p', 'u', 'ul', 'y', 'm', 'o', 'e', 'f', 't', 'w']:
+                    continue
+                
                 select = cursor.execute('SELECT * FROM keywords where word=?',(word,)).fetchall()
                 if select:
                     cursor.execute('UPDATE keywords SET count=? where word=?', (select[0][1] + 1, word))
